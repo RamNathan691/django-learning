c25bbaa24463f40b4cb34a1460b87c1b4e8229be
RamNathan691 <ramnathan0691@gmail.com>
2019-06-08 19:03:33 +0530

    done till the login and logout part

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('insdynamic.urls')),
     path('admin/', admin.site.urls),
+    path('account/',include('account.urls')),
 ]
 urlpatterns =urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
